815f3df3789fdc53e34b46d006625a275676fcb7
uhhhsoyan <eric.essoyan@gmail.com>
2020-12-07 10:19:18 -0800

    hookup left side panel

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http= require('http'); // used by express under the hood! but we want to a
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
-const { userJoin, getCurrentUser } = require('./utils/users');
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -26,6 +26,12 @@ io.on('connection', socket => {
 
     // Broadcast when a user connects
     socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`)); // emits to everyone except user that is connecting
+
+    // Send users and room info
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    })
   })
 
   // Listed for chat message
@@ -37,7 +43,17 @@ io.on('connection', socket => {
 
   // Runs when client disconnects
   socket.on('disconnect', () => {
-    io.emit('message', formatMessage(botName, 'A user has left the chat'));
+    const user = userLeave(socket.id);
+
+    if(user) {
+      io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`));
+
+      // Send users and room info
+      io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room),
+      })
+    }
   })
 
 })
